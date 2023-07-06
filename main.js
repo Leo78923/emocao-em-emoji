b76368fcb9078bbf1c9cdf5128f8e6c3b24d6b28
Leo78923 <117394079+Leo78923@users.noreply.github.com>
2023-07-06 17:45:53 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -37,3 +37,56 @@ function speak()
     var utterThis = new SpeechSynthesisUtterance(speakData1 + speakData2);
     synth.speak(utterThis);
 }
+
+function check()
+{
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results)
+{
+    if(error)
+    {
+        console.error(error);
+    }
+    else
+    {
+        console.log(results);
+        document.getElementById("resultEmotionName").innerHTML = results[0].label;
+        document.getElementById("resultEmotionName2").innerHTML = results[1].label;
+        prediction1 = results[0].label;
+        prediction2 = results[1].label;
+        speak();
+
+        if(results[0].label == "feliz") 
+        {
+            document.getElementById("updateEmoji").innerHTML = "&#128522;";
+        }
+
+        if(results[0].label == "tranquilo") 
+        {
+            document.getElementById("updateEmoji").innerHTML = "&#128526;";
+        }
+
+        if(results[0].label == "bravo") 
+        {
+            document.getElementById("updateEmoji").innerHTML = "&#128548;";
+        }
+
+        if(results[1].label == "feliz") 
+        {
+            document.getElementById("updateEmoji2").innerHTML = "&#128522;";
+        }
+
+        if(results[1].label == "tranquilo") 
+        {
+            document.getElementById("updateEmoji2").innerHTML = "&#128526;";
+        }
+
+        if(results[1].label == "bravo") 
+        {
+            document.getElementById("updateEmoji2").innerHTML = "&#128548;";
+        }
+    }
+}
